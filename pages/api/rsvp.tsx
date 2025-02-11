90fc69c71d77652afb0251974451413544956833
Carlos Zinato <carlosh.lz@hotmail.com>
2025-02-11 01:04:45 +0100

    adds another email

--- pages/api/rsvp.tsx
+++ pages/api/rsvp.tsx
@@ -15,7 +15,7 @@ export default async function handler(
   const {name, people} = req.body;
   const messageData = {
     from: "Lista RSVP <jlzinato@gmail.com>",
-    to: ["cmzinato@gmail.com", "jlzinato@gmail.com"],
+    to: ["cmzinato@gmail.com", "jlzinato@gmail.com", "cavendeiro@gmail.com"],
     subject: `Presença confirmada! ${name}`,
     text: `A ${name} confirmou a presença dela + ${people} pessoas`,
   };
