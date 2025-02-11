70d03f7e2cfb48a58f0f245696158c6d0306fcc8
Carlos Zinato <carlosh.lz@hotmail.com>
2025-02-11 01:01:45 +0100

    firssstttt

---
+++ pages/api/rsvp.tsx
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Mailgun from 'mailgun.js';
+import FormData from 'form-data';
+
+type ResponseData = {
+  message: string
+}
+ 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const mgKey = process.env.MG_API_KEY || '';
+  const mgDomain = process.env.MG_DOMAIN || '';
+  const {name, people} = req.body;
+  const messageData = {
+    from: "Lista RSVP <jlzinato@gmail.com>",
+    to: ["cmzinato@gmail.com", "jlzinato@gmail.com"],
+    subject: `Presença confirmada! ${name}`,
+    text: `A ${name} confirmou a presença dela + ${people} pessoas`,
+  };
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: 'api',
+    key: mgKey
+  });
+  const response = mg.messages.create(mgDomain, messageData);
+  console.log(response);
+  res.status(200).json({ message: 'Success' })
+}
---
+++ src/app/rsvp/page.tsx
@@ -0,0 +1,79 @@
+'use client'
+import { useState } from 'react';
+import Confetti from 'react-confetti'
+
+export default function Rsvp() {
+  const [isSubmitted, setSubmitted] = useState(false)
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState('')
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try {
+      const res = await fetch('/api/rsvp', {
+        method: 'POST',
+        body: JSON.stringify({ name, people }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+      if (res.status === 200) {
+        setSubmitted(true)
+      }
+    } catch (err: any) {
+      console.error('Err', err)
+    }
+  }
+
+  return isSubmitted ? (
+    <div>
+      <h1 className="text-center font-semibold text-3xl mt-4">
+        {`Obrigado ${name}! Sua presença foi confirmada!`}
+      </h1>
+      <Confetti />
+    </div>
+  ) : 
+    <form
+      className="flex flex-col p-4"
+      onSubmit={onSubmit}
+    >
+      {/* Name */}
+      <label className="label font-semibold mt-2" htmlFor='nomeCompleto'>
+        <span className="label-text">Nome completo</span>
+      </label>
+      <input
+        id='nomeCompleto'
+        name='nomeCompleto'
+        className="input w-full input-bordered input-primary mt-2 mb-2 border border-zinc-800 p-1 text-lg"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Digite seu nome"
+      />
+      {/* People */}
+      <label className="label font-semibold" htmlFor='pessoas'>
+        <span className="label-text">Número de acompanhantes</span>
+      </label>
+      <input
+        id='pessoas'
+        name='pessoas'
+        className="w-full mt-2 mb-2 border border-zinc-800 p-1 text-lg"
+        type='number'
+        inputMode={'numeric'}
+        min={1}
+        max={20}
+        step={1}
+        placeholder="2"
+        value={people}
+        onChange={(e) => setPeople(e.target.value)}
+      />
+      {/* Button */}
+      <button
+        className="text-white bg-gradient-to-br mt-4 from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-semibold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        type="submit">
+          Confirmar presença
+      </button>
+    </form>
+  
+};
