363de52f312c9ad55b84c37487fec4fbf9eb689c
Carlos Zinato <carlosh.lz@hotmail.com>
2025-02-16 20:14:52 +0100

    back arrow links to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,20 @@
 'use client'
+import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
-import { useRouter } from 'next/navigation'
 
 type HeaderProps = {
   hideBack?: boolean;
 };
 const Header = ({ hideBack }: HeaderProps) => {
-  const router = useRouter();
   return (
     <div className='flex flex-col p-1 max-w-4xl'>
       {!hideBack && (
-        <ArrowLeft
-          className='cursor-pointer'
-          size={30}
-          onClick={() => {
-            router.back();
-          }}
-        />
+        <Link href={'/'}>
+          <ArrowLeft
+            className='cursor-pointer'
+            size={30}
+          />
+        </Link>
       )}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img className={`w-auto`} src={'/logo.png'} alt='Carol & Joao'/>
