37665866228a87fefb50a794afb838dc2d617335
Carlos Zinato <carlosh.lz@hotmail.com>
2025-02-23 14:54:22 +0100

    new feedback mechanism

--- src/app/presentes/page.tsx
+++ src/app/presentes/page.tsx
@@ -12,20 +12,13 @@ const pixValueJoao = `+5511994524358`;
 const pixValueCarol = `+5511992294062`;
 const pixJoao = `Chave Pix: +5511994524358`;
 const pixCarol = `Chave Pix: +5511992294062`;
+
 export default function Presentes() {
   const [showAlert, setShowAlert] = useState(false);
+  const [showAlert2, setShowAlert2] = useState(false);
+
   return (
     <div className="p-4 flex flex-col items-center">
-      {showAlert && (
-        <div
-          className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md"
-          role="alert"
-        >
-          <span className="font-semibold mr-2 text-left flex-auto">
-            Chave Pix copiada
-          </span>
-        </div>
-      )}
       <Header />
       <p className="text-center mt-8 mb-8 font-[family-name:var(--font-geist-sans)] font-thin ml-1">
         {texto}
@@ -37,19 +30,31 @@ export default function Presentes() {
             {tituloJoao}
           </p>
           <Image src="/QRJoao.jpg" width={250} height={250} alt="Pix QR Code" />
-          <p
-            className="text-center mt-6 mb-8 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
-            onClick={() => {
-              navigator.clipboard.writeText(`${pixValueJoao}`);
-              setShowAlert(true);
-              setTimeout(() => {
-                setShowAlert(false);
-              }, 2000);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            {pixJoao}
-          </p>
+          <div className="flex flex-col mb-8">
+            <p
+              className="text-center mt-6 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
+              onClick={() => {
+                navigator.clipboard.writeText(`${pixValueJoao}`);
+                setShowAlert(true);
+                setTimeout(() => {
+                  setShowAlert(false);
+                }, 2000);
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              {pixJoao}
+            </p>
+            {showAlert && (
+              <div
+                className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md mt-1"
+                role="alert"
+              >
+                <span className="font-semibold mr-2 text-left flex-auto">
+                  Chave Pix copiada
+                </span>
+              </div>
+            )}
+          </div>
         </div>
         <div>
           <p className="text-center mb-2 font-[family-name:var(--font-geist-sans)] font-normal text-lg">
@@ -61,19 +66,31 @@ export default function Presentes() {
             height={250}
             alt="Pix QR Code"
           />
-          <p
-            className="text-center mt-6 mb-8 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
-            onClick={() => {
-              navigator.clipboard.writeText(`${pixValueCarol}`);
-              setShowAlert(true);
-              setTimeout(() => {
-                setShowAlert(false);
-              }, 2000);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            {pixCarol}
-          </p>
+          <div className="flex flex-col mb-8">
+            <p
+              className="text-center mt-6 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
+              onClick={() => {
+                navigator.clipboard.writeText(`${pixValueCarol}`);
+                setShowAlert2(true);
+                setTimeout(() => {
+                  setShowAlert2(false);
+                }, 2000);
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              {pixCarol}
+            </p>
+            {showAlert2 && (
+              <div
+                className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md mt-1"
+                role="alert"
+              >
+                <span className="font-semibold mr-2 text-left flex-auto">
+                  Chave Pix copiada
+                </span>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
