aa993b175bd3c31198d210e6b80c4e52cdd07199
Carlos Zinato <carlosh.lz@hotmail.com>
2025-04-17 11:22:45 +0200

    dados bancarios da carol

--- src/app/presentes/page.tsx
+++ src/app/presentes/page.tsx
@@ -12,30 +12,50 @@ const pixValueJoao = `+5511994524358`;
 const pixValueCarol = `+5511992294062`;
 const pixJoao = `Chave Pix: +5511994524358`;
 const pixCarol = `Chave Pix: +5511992294062`;
-const bancoTitulo = `Se preferir, você também pode fazer uma transferência bancária`;
+
+const bancoTitulo = `Dados bancários`;
 const bancoLabel = `Banco:`;
-const bancoDescription = `077 - Inter`;
+const bancoDescriptionJoao = `077 - Inter`;
+const bancoDescriptionCarol = `0260 - Nu Pagamentos S.A.`;
 const contaLabel = `Número da conta:`;
-const contaDescription = `3362216-7`;
+const contaDescriptionJoao = `3362216-7`;
+const contaDescriptionCarol = `6046626-3`;
 const agenciaLabel = `Agência:`;
-const agenciaDescription = `0001`;
+const agenciaDescriptionJoao = `0001`;
+const agenciaDescriptionCarol = `0001`;
 const cpfLabel = `CPF:`;
-const cpfDescription = `294.667.828-86`;
+const cpfDescriptionJoao = `294.667.828-86`;
+const cpfDescriptionCarol = `339.279.508-90`;
 
 export default function Presentes() {
   const [showAlert, setShowAlert] = useState(false);
   const [showAlert2, setShowAlert2] = useState(false);
   const [showAlert3, setShowAlert3] = useState(false);
   const [alert3Message, setAlert3Message] = useState("");
+  const [showAlert4, setShowAlert4] = useState(false);
+  const [alert4Message, setAlert4Message] = useState("");
 
-  const copyBankDetails = (itemToCopy: string, message: string) => {
+  const copyBankDetails = (
+    itemToCopy: string,
+    message: string,
+    above: boolean = true
+  ) => {
     navigator.clipboard.writeText(`${itemToCopy}`);
-    setAlert3Message(message);
-    setShowAlert3(true);
-    setTimeout(() => {
-      setShowAlert3(false);
-      setAlert3Message("");
-    }, 2000);
+    if (above) {
+      setAlert3Message(message);
+      setShowAlert3(true);
+      setTimeout(() => {
+        setShowAlert3(false);
+        setAlert3Message("");
+      }, 2000);
+    } else {
+      setAlert4Message(message);
+      setShowAlert4(true);
+      setTimeout(() => {
+        setShowAlert4(false);
+        setAlert4Message("");
+      }, 2000);
+    }
   };
 
   return (
@@ -45,13 +65,13 @@ export default function Presentes() {
         {texto}
       </p>
       {/* Container */}
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center">
         <div>
           <p className="text-center mb-2 font-[family-name:var(--font-geist-sans)] font-normal text-lg">
             {tituloJoao}
           </p>
           <Image src="/QRJoao.jpg" width={250} height={250} alt="Pix QR Code" />
-          <div className="flex flex-col mb-8">
+          <div className="flex flex-col mb-2">
             <p
               className="text-center mt-6 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
               onClick={() => {
@@ -76,10 +96,71 @@ export default function Presentes() {
               </div>
             )}
           </div>
+          {/* Banco João */}
+          <div className="flex flex-col items-center justify-center mt-4 mb-4">
+            <p className="text-center font-[family-name:var(--font-geist-sans)] font-normal">
+              {bancoTitulo}
+            </p>
+            <div className="border rounded-md flex flex-col p-2 mb-2 mt-2">
+              <span
+                className="text-left flex-auto mb-1 flex flex-row items-center"
+                onClick={() => {
+                  copyBankDetails(
+                    contaDescriptionJoao,
+                    `Número da conta copiado`
+                  );
+                }}
+              >
+                <Copy className="cursor-pointer mr-1" size={16} />
+                <b className="mr-1">{contaLabel}</b> {contaDescriptionJoao}
+              </span>
+              <span
+                className="text-left flex-auto mb-1 flex flex-row items-center"
+                onClick={() => {
+                  copyBankDetails(
+                    agenciaDescriptionJoao,
+                    `Número da agência copiado`
+                  );
+                }}
+              >
+                <Copy className="cursor-pointer mr-1" size={16} />
+                <b className="mr-1">{agenciaLabel}</b> {agenciaDescriptionJoao}
+              </span>
+              <span
+                className="text-left flex-auto mb-1 flex flex-row items-center"
+                onClick={() => {
+                  copyBankDetails(bancoDescriptionJoao, `Banco copiado`);
+                }}
+              >
+                <Copy className="cursor-pointer mr-1" size={16} />
+                <b className="mr-1">{bancoLabel}</b> {bancoDescriptionJoao}
+              </span>
+              <span
+                className="text-left flex-auto mb-1 flex flex-row items-center"
+                onClick={() => {
+                  copyBankDetails(cpfDescriptionJoao, `CPF copiado`);
+                }}
+              >
+                <Copy className="cursor-pointer mr-1" size={16} />
+                <b className="mr-1">{cpfLabel}</b> {cpfDescriptionJoao}
+              </span>
+            </div>
+            {showAlert3 && (
+              <div
+                className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md mt-1"
+                role="alert"
+              >
+                <span className="font-semibold mr-2 text-left flex-auto">
+                  {alert3Message}
+                </span>
+              </div>
+            )}
+          </div>{" "}
+          {/* Fim Banco João */}
         </div>
         {/* Carol */}
         <div>
-          <p className="text-center mb-2 font-[family-name:var(--font-geist-sans)] font-normal text-lg">
+          <p className="text-center mt-4 mb-2 font-[family-name:var(--font-geist-sans)] font-normal text-lg">
             {tituloCarol}
           </p>
           <Image
@@ -88,7 +169,7 @@ export default function Presentes() {
             height={250}
             alt="Pix QR Code"
           />
-          <div className="flex flex-col mb-8">
+          <div className="flex flex-col mb-2">
             <p
               className="text-center mt-6 font-[family-name:var(--font-geist-sans)] font-thin flex flex-row items-center cursor-pointer"
               onClick={() => {
@@ -114,60 +195,68 @@ export default function Presentes() {
             )}
           </div>
         </div>
-      </div>
-      {/* Banco */}
-      <div>
-        <p className="text-center font-[family-name:var(--font-geist-sans)] font-normal text-lg">
-          {bancoTitulo}
-        </p>
-        <div className="border rounded-md flex flex-col p-2 mb-2 mt-4">
-          <span
-            className="text-left flex-auto mb-1 flex flex-row items-center"
-            onClick={() => {
-              copyBankDetails(contaDescription, `Número da conta copiado`);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            <b className="mr-1">{contaLabel}</b> {contaDescription}
-          </span>
-          <span
-            className="text-left flex-auto mb-1 flex flex-row items-center"
-            onClick={() => {
-              copyBankDetails(agenciaDescription, `Número da agência copiado`);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            <b className="mr-1">{agenciaLabel}</b> {agenciaDescription}
-          </span>
-          <span
-            className="text-left flex-auto mb-1 flex flex-row items-center"
-            onClick={() => {
-              copyBankDetails(bancoDescription, `Banco copiado`);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            <b className="mr-1">{bancoLabel}</b> {bancoDescription}
-          </span>
-          <span
-            className="text-left flex-auto mb-1 flex flex-row items-center"
-            onClick={() => {
-              copyBankDetails(cpfDescription, `CPF copiado`);
-            }}
-          >
-            <Copy className="cursor-pointer mr-1" size={16} />
-            <b className="mr-1">{cpfLabel}</b> {cpfDescription}
-          </span>
-        </div>
-        {showAlert3 && (
-          <div
-            className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md mt-1"
-            role="alert"
-          >
-            <span className="font-semibold mr-2 text-left flex-auto">
-              {alert3Message}
+        {/* Banco Carol */}
+        <div className="flex flex-col items-center justify-center mt-4 mb-4">
+          <p className="text-center font-[family-name:var(--font-geist-sans)] font-normal">
+            {bancoTitulo}
+          </p>
+          <div className="border rounded-md flex flex-col p-2 mb-2 mt-2">
+            <span
+              className="text-left flex-auto mb-1 flex flex-row items-center"
+              onClick={() => {
+                copyBankDetails(
+                  contaDescriptionCarol,
+                  `Número da conta copiado`,
+                  false
+                );
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              <b className="mr-1">{contaLabel}</b> {contaDescriptionCarol}
+            </span>
+            <span
+              className="text-left flex-auto mb-1 flex flex-row items-center"
+              onClick={() => {
+                copyBankDetails(
+                  agenciaDescriptionCarol,
+                  `Número da agência copiado`,
+                  false
+                );
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              <b className="mr-1">{agenciaLabel}</b> {agenciaDescriptionCarol}
+            </span>
+            <span
+              className="text-left flex-auto mb-1 flex flex-row items-center"
+              onClick={() => {
+                copyBankDetails(bancoDescriptionCarol, `Banco copiado`, false);
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              <b className="mr-1">{bancoLabel}</b> {bancoDescriptionCarol}
+            </span>
+            <span
+              className="text-left flex-auto mb-1 flex flex-row items-center"
+              onClick={() => {
+                copyBankDetails(cpfDescriptionCarol, `CPF copiado`, false);
+              }}
+            >
+              <Copy className="cursor-pointer mr-1" size={16} />
+              <b className="mr-1">{cpfLabel}</b> {cpfDescriptionCarol}
             </span>
           </div>
-        )}
+          {showAlert4 && (
+            <div
+              className="p-2 bg-slate-900 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex rounded-md mt-1"
+              role="alert"
+            >
+              <span className="font-semibold mr-2 text-left flex-auto">
+                {alert4Message}
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
